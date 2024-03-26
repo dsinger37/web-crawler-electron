3afe5a7c348f5738ddb1d808d06bd7a7486da750
David Singer <dsinger@digitalmagistery.com>
2024-03-26 09:53:58 -0700

    fix: parse the sitemap ourselves if Crawlee can't
    Sometimes Crawlee won't parse a sitemap. When this happens it gives us a warning in the terminal that the sitemap is malformed and returns an empty URL array. If this happens we now fetch and parse the sitemap with a custom function.
    
    Fixes: #CRAWLER-14

--- src/sitemapUtils.ts
+++ src/sitemapUtils.ts
@@ -1,6 +1,29 @@
 import { Sitemap } from "crawlee";
+import { XMLParser } from "fast-xml-parser";
 
-const sitemapUrlsToCheck = ["/sitemap_index.xml", "/sitemap.xml", "/sitemap"];
+async function countUrlsInSitemap(sitemapUrl: string): Promise<number> {
+  const response = await fetch(sitemapUrl);
+  const xmlData = await response.text();
+
+  const parserOptions = {
+    ignoreAttributes: false,
+    attributeNamePrefix: "",
+  };
+  const parser = new XMLParser(parserOptions);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch sitemap: ${response.statusText}`);
+  }
+
+  const jsonObj = parser.parse(xmlData);
+
+  if (!jsonObj.urlset || !jsonObj.urlset.url) {
+    console.error("Invalid sitemap format");
+    return 0;
+  }
+
+  return Array.isArray(jsonObj.urlset.url) ? jsonObj.urlset.url.length : 1;
+}
 
 async function checkSitemapExists(baseUrl: string, sitemapPath: string): Promise<boolean> {
   const url = `${baseUrl}${sitemapPath}`;
@@ -19,6 +42,7 @@ async function loadSitemap(url: string): Promise<Sitemap | null> {
 }
 
 export async function checkAndParseSitemap(websiteUrl: string): Promise<number> {
+  const sitemapUrlsToCheck = ["/sitemap_index.xml", "/sitemap.xml", "/sitemap"];
   try {
     for (const sitemapPath of sitemapUrlsToCheck) {
       const sitemapUrl = `${websiteUrl}${sitemapPath}`;
@@ -28,6 +52,10 @@ export async function checkAndParseSitemap(websiteUrl: string): Promise<number> 
         const sitemap = await loadSitemap(sitemapUrl);
 
         if (sitemap) {
+          // Sometimes Crawlee will not parse the sitemap correctly, so we need to check if it has any URLs
+          if (sitemap.urls.length === 0) {
+            return countUrlsInSitemap(sitemapUrl);
+          }
           return sitemap.urls.length;
         }
       }
